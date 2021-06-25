831a96840b9cf537d13b4b83cbb7f1477b810b54
Jonghyeon Ko <flowithusdev@gmail.com>
2021-06-25 23:13:06 +0900

    Navigating Programmatically

--- pages/clients/[id]/index.js
+++ pages/clients/[id]/index.js
@@ -5,10 +5,20 @@ function ClientProjectsPage() {
 
   console.log(router.query);
 
+  const loadProjectHandler = () => {
+    // load data...
+    router.push({
+      pathname: '/clients/[id]/[clientprojectid]',
+      query: { id: router.query.id, clientprojectid: 'projecta' },
+    });
+  };
+
   return (
     <div>
       <h1>The Projects of a Given Client</h1>
       <h2>{router.query.id}</h2>
+
+      <button onClick={loadProjectHandler}>Load Project A</button>
     </div>
   );
 }
