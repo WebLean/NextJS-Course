a52a98f159515d6f0073b55400f95a0ea431bf11
Jonghyeon Ko <flowithusdev@gmail.com>
2021-06-27 00:26:24 +0900

    Adding Icons
    https://heroicons.com/

--- components/events/EventItem.js
+++ components/events/EventItem.js
@@ -1,6 +1,9 @@
-import Link from 'next/Link';
+import Image from 'next/image';
 import classes from './EventItem.module.css';
 import Button from '../../ui/button';
+import DateIcon from '../icons/DateIcon';
+import AddressIcon from '../icons/AddressIcon';
+import ArrowRightIcon from '../icons/ArrowRightIcon';
 
 function EventItem({ item }) {
   const { title, image, date, location, id } = item;
@@ -17,19 +20,32 @@ function EventItem({ item }) {
 
   return (
     <li className={classes.item}>
-      <img src={'/' + image} alt={title} />
+      <Image
+        width="300"
+        height="100"
+        objectFit="cover"
+        src={'/' + image}
+        alt={title}
+      />
       <div className={classes.content}>
         <div className={classes.summary}>
           <h2>{title}</h2>
           <div className={classes.date}>
+            <DateIcon />
             <time>{humanReadableDate}</time>
           </div>
           <div className={classes.address}>
+            <AddressIcon />
             <address>{formattedAddress}</address>
           </div>
         </div>
         <div className={classes.actions}>
-          <Button link={exploreLink}>Explore Event</Button>
+          <Button link={exploreLink}>
+            <span>Explore Event</span>
+            <span className={classes.icon}>
+              <ArrowRightIcon />
+            </span>
+          </Button>
         </div>
       </div>
     </li>
